11c78cec4943abf31254681669602aeff8b8160a
liuhao1088 <1350569467@qq.com>
2020-09-30 14:30:14 +0800

    更新

--- pages/detailsawTwo/detailsawTwo.js
+++ pages/detailsawTwo/detailsawTwo.js
@@ -8,21 +8,17 @@ Page({
     top:"4%",
   },
   fanhuidj:function(){
-    wx.reLaunch({
-       url: "../index/index"
-     })
- },
+    wx.navigateTo({
+      url: "../hiddenRecorderSeries/hiddenRecorderSeries"
+    })
+  },
 
  zhuyefanhuidj:function(){
   wx.reLaunch({
      url: "../index/index"
    })
-},
-detailsawTwodj:function(){
-  wx.navigateTo({
-    url: "../detailsawTwo/detailsawTwo"
-  })
-},
+  },
+ 
   /**
    * 生命周期函数--监听页面加载
    */
