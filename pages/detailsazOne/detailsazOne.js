0fbe6c29ed02cd3a92445c17b4b548dd3fa426b3
liuhao1088 <1350569467@qq.com>
2020-10-09 11:48:10 +0800

    更新

--- pages/detailsazOne/detailsazOne.js
+++ pages/detailsazOne/detailsazOne.js
@@ -51,7 +51,13 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+    wx.showShareMenu({
 
+      withShareTicket:true,
+      
+      menus:['shareAppMessage','shareTimeline']
+      
+      })
   },
 
   /**
