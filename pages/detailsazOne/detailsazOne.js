14fd9f367b711d8484fddb444047068de758292d
liuhao1088 <1350569467@qq.com>
2021-07-07 09:51:00 +0800

    +修改

--- pages/detailsazOne/detailsazOne.js
+++ pages/detailsazOne/detailsazOne.js
@@ -8,9 +8,9 @@ Page({
     item: {
       topUrl: 'https://p.pstatp.com/origin/137ab0001f0249415d52e',
       top:'58%',
-      videoUrl:'https://fnck-20200914-1303154931.cos.ap-guangzhou.myqcloud.com/%E8%A7%86%E9%A2%91/%E4%BA%94%E6%B4%B2%E8%A1%8CZ1%E5%A4%96%E8%A7%82.mp4?q-sign-algorithm=sha1&q-ak=AKIDHL7fPrFwXrtdX9SYLq75j5BxTeFbR3KK&q-sign-time=1601450307;33137450307&q-key-time=1601450307;33137450307&q-header-list=&q-url-param-list=&q-signature=f5e0d8f97b129e81efe2415952a35b206fa9ec2d',
+      videoUrl:'',
       detailUrl:'https://p.pstatp.com/origin/fe71000318f5f1066cf8'
-
+      // https://fnck-20200914-1303154931.cos.ap-guangzhou.myqcloud.com/%E8%A7%86%E9%A2%91/%E4%BA%94%E6%B4%B2%E8%A1%8CZ1%E5%A4%96%E8%A7%82.mp4?q-sign-algorithm=sha1&q-ak=AKIDHL7fPrFwXrtdX9SYLq75j5BxTeFbR3KK&q-sign-time=1601450307;33137450307&q-key-time=1601450307;33137450307&q-header-list=&q-url-param-list=&q-signature=f5e0d8f97b129e81efe2415952a35b206fa9ec2d
     },
   },
  
